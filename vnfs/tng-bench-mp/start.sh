6d1e7a376d375ea69edbda2acefc945dca339b22
root <arif332@gmail.com>
2019-09-22 00:00:24 +0200

    tng-bench-mp: add vaaa test script

--- vnfs/tng-bench-mp/start.sh
+++ vnfs/tng-bench-mp/start.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 date > /tngbench_share/start_time.txt
+/vaaa-test.sh >/tngbench_share/vaaa-test.log
+
 #eval "$start_command"
---
+++ vnfs/tng-bench-mp/vaaa-test.sh
@@ -0,0 +1,2 @@
+
+radtest bob test 20.0.0.254 0 testing123
