61069b27e6cf3f0b1a53704cd907fcabeaf1a70d
root <arif332@gmail.com>
2019-10-20 22:54:27 +0200

    add ims-client-mp

---
+++ vnfs/ims-client-mp/start.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+date > /tngbench_share/start_time.txt
+ip a s>/tngbench_share/system.log
+ip r s>>/tngbench_share/system.log
+uptime>>/tngbench_share/system.log
+
+#radius authentication verification log
+#/vaaa-test.sh >/tngbench_share/vaaa-user-verification.log
+
+#eval "$start_command"
