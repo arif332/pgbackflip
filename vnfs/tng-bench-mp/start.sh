90ded5038e2170ef169229ce9c490b14323cff35
root <arif332@gmail.com>
2019-11-24 18:23:29 +0100

    update start script

--- vnfs/tng-bench-mp/start.sh
+++ vnfs/tng-bench-mp/start.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 date > /tngbench_share/start_time.txt
-#ip a s>/tngbench_share/system.log
-#ip r s>>/tngbench_share/system.log
-#uptime>>/tngbench_share/system.log
+
+wait 2
 
 #radius authentication verification log
 /vaaa-test.sh >/tngbench_share/vaaa-user-verification.log
--- vnfs/vAAA/start.sh
+++ vnfs/vAAA/start.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 date > /tngbench_share/start.txt
 
-/etc/init.d/freeradius start >freeradius_server_start.log
-#ip a s >>/tngbench_share/system.log
-#ip r s >>/tngbench_share/system.log
-#ps -ef >>/tngbench_share/system.log
-#/etc/init.d/freeradius start>>/tngbench_share/system.log
-#radtest bob test 127.0.0.1 0 testing123>>/tngbench_share/system.log
+/etc/init.d/freeradius start >/tngbench_share/freeradius_server_start.log
 
-#cp /var/log/freeradius/radius.log /tngbench_share/radius.log
-#cp /var/log/ /tngbench_share/radius.log
+
 
