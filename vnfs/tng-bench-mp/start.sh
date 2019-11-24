31243d6897550a4f4feba18e651c031e6f8d7136
root <arif332@gmail.com>
2019-11-24 16:59:39 +0100

    update vaaa config

--- vnfs/tng-bench-mp/start.sh
+++ vnfs/tng-bench-mp/start.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 date > /tngbench_share/start_time.txt
-ip a s>/tngbench_share/system.log
-ip r s>>/tngbench_share/system.log
-uptime>>/tngbench_share/system.log
+#ip a s>/tngbench_share/system.log
+#ip r s>>/tngbench_share/system.log
+#uptime>>/tngbench_share/system.log
 
 #radius authentication verification log
 /vaaa-test.sh >/tngbench_share/vaaa-user-verification.log
--- vnfs/tng-bench-mp/stop.sh
+++ vnfs/tng-bench-mp/stop.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
 date > /tngbench_share/stop_time.txt
+
+#genreate result.yml
+python ./log_intf_statistics.py /tngbench_share/result.yml
+
